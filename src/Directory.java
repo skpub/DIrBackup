644c6cb746b8ac807c14b4096676f2af539c37fb
Sato Kaito <satodeyannsu@gmail.com>
2023-05-11 16:55:28 +0900

    implementing sub

--- src/Directory.java
+++ src/Directory.java
@@ -2,10 +2,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.attribute.FileTime;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Directory {
     Path path;
@@ -13,7 +10,7 @@ public class Directory {
 
     Directory(Path path) {
         this.path = path.toAbsolutePath();
-        this.structure = new DirectoryDCP(path);
+        this.structure = new DirectoryDCP(path, true);
     }
 
     Directory(Path path, DirectoryDCP dirDCP) {
@@ -22,84 +19,271 @@ public class Directory {
     }
 
     public Directory sub(Directory b) {
-        DirectoryDCP temp_str = new DirectoryDCP(this.path.toString());
-        Directory temp = new Directory(this.path, temp_str);
+        DirectoryDCP temp_structure = new DirectoryDCP(this.path, false);
+        Directory temp = new Directory(this.path, temp_structure);
         return new Directory(
             this.path,
-            sub_(this.path, b.path, this.structure, b.structure, temp.structure)
+//            sub_(this.path, b.path, this.structure, Optional.ofNullable(b.structure), temp.structure)
+            sub__(
+                this.path,
+                Optional.ofNullable(b.path),
+                this.structure,
+                Optional.ofNullable(b.structure),
+                temp.structure).get()
         );
+    }
+
+
+    public static Optional<DirectoryDCP> sub__(
+        Path a_root,
+        Optional<Path> b_root_opt,
+        DirectoryDCP a_ptr,
+        Optional<DirectoryDCP> b_ptr_opt,
+        DirectoryDCP parent
+    ) {
+        a_ptr.dirs.ifPresentOrElse(
+            a_dirs -> {
+                b_ptr_opt.ifPresentOrElse(
+                    b_ptr -> {
+                        b_root_opt.ifPresent( b_root ->
+                            b_ptr.dirs.ifPresentOrElse(
+                                b_dirs -> {
+                                    a_dirs.forEach((path, a_dir) -> {
+                                        System.out.println("来たよ");
+                                        Optional.ofNullable(b_dirs.get(path)).ifPresentOrElse(
+                                            b_dir -> {
+                                                DirectoryDCP next_parent = new DirectoryDCP(path, false);
+                                                Optional<DirectoryDCP> child_opt = sub__(
+                                                    a_root.resolve(path),
+                                                    Optional.of(b_root.resolve(path)),
+                                                    a_dir,
+                                                    Optional.of(b_dir),
+                                                    next_parent
+                                                );
+                                                child_opt.ifPresent(parent::addDirectory);
+                                            },
+                                            () -> {
+                                                parent.addDirectory(a_dir);
+                                                DirectoryDCP next_parent = new DirectoryDCP(path, false);
+//                                                Optional<DirectoryDCP> child_opt = sub__(
+//
+//                                                );
+                                            }
+                                        );
+                                    });
+                                },
+                                () -> { // b_ptr.dirs is empty.
+                                }
+                            )
+                        );
+                    },
+                    () -> { // b_ptr is null.
+                    }
+                );
+            },
+            () -> { // a_ptr.dirs is empty.
+//                a_ptr.files.ifPresentOrElse(
+//                    a_files -> {
+//                        a_files.forEach(parent::addFile);
+//                    },
+//                    () -> {
+//                    }
+//                );
+            }
+        );
+        System.out.println("parent: " + parent.name);
+        a_ptr.files.ifPresent(
+            a_files -> {
+                b_ptr_opt.ifPresentOrElse(
+                    b_ptr ->
+                        b_root_opt.ifPresentOrElse(
+                            b_root -> {
+                                b_ptr.files.ifPresentOrElse(
+                                    b_files -> {
+                                        a_files.forEach(file -> {
+                                            if (b_files.contains(file)) {
+                                                Path a_abs = a_root.resolve(file);
+                                                Path b_abs = b_root.resolve(file);
+                                                FileTime time_a;
+                                                FileTime time_b;
+                                                try {
+                                                    time_a = Files.getLastModifiedTime(a_abs);
+                                                    time_b = Files.getLastModifiedTime(b_abs);
+                                                } catch (IOException e) {
+                                                    throw new RuntimeException(e);
+                                                }
+                                                if (time_a.compareTo(time_b) > 0) {
+                                                    parent.addFile(file);
+                                                }
+                                            } else {
+                                                parent.addFile(file);
+                                            }
+                                        });
+                                    },
+                                    () -> { // b_files is empty.
+                                        a_files.forEach(parent::addFile);
+                                    }
+                                );
+                            },
+                            () -> {
+
+                            }
+                        ),
+                    () -> {
+                        a_files.forEach(parent::addFile);
+                    }
+                );
+            }
+        );
+        return Optional.ofNullable(parent);
     }
 
     public static DirectoryDCP sub_(
         Path a_root,
         Path b_root,
         DirectoryDCP a_ptr,
-        DirectoryDCP b_ptr,
+        Optional<DirectoryDCP> b_ptr_opt,
         DirectoryDCP temp)
     {
         System.out.println("a_root: " + a_root.toString());
         System.out.println("b_root: " + b_root.toString());
         System.out.println("a_ptr: " + a_ptr.name.toString());
-        System.out.println("b_ptr: " + b_ptr.name.toString());
+        b_ptr_opt.ifPresentOrElse(
+            b_ptr -> System.out.println("b_ptr: " + b_ptr.name.toString()),
+            () -> System.out.println("b_ptr: NULL")
+        );
+        a_ptr.dirs.ifPresentOrElse(
+            a_dirs -> {
+                b_ptr_opt.ifPresentOrElse(
+                    b_ptr -> {
+                        b_ptr.dirs.ifPresentOrElse(
+                            b_dirs -> {
+                                a_dirs.forEach((path, a_dir) -> {
+                                    Optional.ofNullable(b_dirs.get(path)).ifPresentOrElse(
+                                        b_dir -> {
+                                            Path a_abs = a_root.resolve(a_dir.name);
+                                            Path b_abs = b_root.resolve(b_dir.name);
+                                            FileTime time_a;
+                                            FileTime time_b;
+                                            try {
+                                                time_a = Files.getLastModifiedTime(a_abs);
+                                                time_b = Files.getLastModifiedTime(b_abs);
+                                            } catch (IOException e) {
+                                                throw new RuntimeException(e);
+                                            }
+                                            if (time_a.compareTo(time_b) > 0) {
+                                                temp.addDirectory(b_dir);
+                                            }
+                                        },
+                                        () -> {
 
-        temp.files = new HashSet<>();
-        a_ptr.files.forEach(file -> {
-            if (b_ptr.files.contains(file)) {
-                Path a_abs = a_root.resolve(file);
-                Path b_abs = b_root.resolve(file);
-                FileTime time_a;
-                FileTime time_b;
-                try {
-                    time_a = Files.getLastModifiedTime(a_abs);
-                    time_b = Files.getLastModifiedTime(b_abs);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-                if (time_a.compareTo(time_b) > 0) {
-                    temp.files.add(file);
-                }
-            } else {
-                temp.files.add(file);
-            }
-        });
-        if (a_ptr.dirs.isEmpty()) {
-            return temp;
-        } else {
-            Map<Path, DirectoryDCP> current_dirs = new HashMap<>();
-            a_ptr.dirs.forEach((path, dir) -> { // dir: Path (key)
-                Path a_abs = a_root.resolve(dir.name);
-                Path b_abs = b_root.resolve(dir.name);
-                DirectoryDCP b_dir;
-                if ((b_dir = b_ptr.dirs.get(path)) != null && b_ptr.dirs != null) {
-                    FileTime time_a;
-                    FileTime time_b;
-                    try {
-                        time_a = Files.getLastModifiedTime(a_abs);
-                        time_b = Files.getLastModifiedTime(b_abs);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                    if (time_a.compareTo(time_b) > 0) {
-                        current_dirs.put(path, dir);
-                        sub_(
-                            a_abs,
-                            b_abs,
-                            a_ptr.dirs.get(path),
-                            b_ptr.dirs.get(path),
-                            current_dirs.get(path)
+                                        }
+                                    );
+
+                                });
+                            },
+                            () -> {
+                                a_dirs.forEach((path, dir) -> {
+                                    temp.addDirectory(dir);
+                                    sub_(
+                                        a_root,
+                                        b_root,
+                                        dir,
+                                        Optional.empty(),
+                                        dir
+                                    );
+                                });
+                            }
                         );
-                    } else {
-                        return;
+
+                    },
+                    () -> {
+
                     }
-                } else {
-                    current_dirs.put(path, dir);
-                    sub_( a_abs,
-                        b_abs,
-                        a_ptr.dirs.get(path),
-                        b_ptr.dirs.get(path),
-                        current_dirs.get(path)
-                    );
-                }
+                );
+            },
+            () -> {
+            }
+        );
+
+        System.out.println("END: DIR");
+
+        a_ptr.files.ifPresent(
+            a_files -> {
+                b_ptr_opt.ifPresentOrElse(
+                    b_ptr ->
+                        b_ptr.files.ifPresentOrElse(
+                            b_files -> {
+                                a_files.forEach(file -> {
+                                    if (b_files.contains(file)) {
+                                        Path a_abs = a_root.resolve(file);
+                                        Path b_abs = b_root.resolve(file);
+                                        FileTime time_a;
+                                        FileTime time_b;
+                                        try {
+                                            time_a = Files.getLastModifiedTime(a_abs);
+                                            time_b = Files.getLastModifiedTime(b_abs);
+                                        } catch (IOException e) {
+                                            throw new RuntimeException(e);
+                                        }
+                                        if (time_a.compareTo(time_b) > 0) {
+                                            temp.addFile(file);
+                                        }
+                                    } else {
+                                        temp.addFile(file);
+                                    }
+                                });
+                                },
+                            () -> { // b_files is empty.
+                                a_files.forEach(temp::addFile);
+                            }
+                        ),
+                    () -> {
+                        a_files.forEach(temp::addFile);
+                    }
+                );
+            }
+        );
+
+
+//        if (a_ptr.dirs.isEmpty()) {
+//            return temp;
+//        } else {
+//            Map<Path, DirectoryDCP> current_dirs = new HashMap<>();
+//            a_ptr.dirs.forEach((path, dir) -> { // dir: Path (key)
+//                Path a_abs = a_root.resolve(dir.name);
+//                Path b_abs = b_root.resolve(dir.name);
+//                DirectoryDCP b_dir;
+//                if ((b_dir = b_ptr.dirs.get(path)) != null && b_ptr.dirs != null) {
+//                    FileTime time_a;
+//                    FileTime time_b;
+//                    try {
+//                        time_a = Files.getLastModifiedTime(a_abs);
+//                        time_b = Files.getLastModifiedTime(b_abs);
+//                    } catch (IOException e) {
+//                        throw new RuntimeException(e);
+//                    }
+//                    if (time_a.compareTo(time_b) > 0) {
+//                        current_dirs.put(path, dir);
+//                        sub_(
+//                            a_abs,
+//                            b_abs,
+//                            a_ptr.dirs.get(path),
+//                            b_ptr.dirs.get(path),
+//                            current_dirs.get(path)
+//                        );
+//                    } else {
+//                        return;
+//                    }
+//                } else {
+//                    current_dirs.put(path, dir);
+//                    sub_( a_abs,
+//                        b_abs,
+//                        a_ptr.dirs.get(path),
+//                        b_ptr.dirs.get(path),
+//                        current_dirs.get(path)
+//                    );
+//                }
 
 
 
@@ -128,8 +312,8 @@ public class Directory {
 ////                        )
 //                    }
 //                }
-            });
-        }
+//            });
+//        }
         return temp;
     }
 
--- src/DirectoryDCP.java
+++ src/DirectoryDCP.java
@@ -5,38 +5,77 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.*;
+import java.util.Optional;
 
 public class DirectoryDCP implements Comparable<DirectoryDCP> {
     Path name;
-    Set<Path> files = new HashSet<>();
-    Map<Path, DirectoryDCP> dirs = new HashMap<>();
+    Optional<Set<Path>> files;
+    Optional<Map<Path, DirectoryDCP>> dirs;
 
-    DirectoryDCP() {}
-    DirectoryDCP(String name) {
-        this.name = Paths.get(name);
+    DirectoryDCP() {
+        this.files = Optional.empty();
+        this.dirs = Optional.empty();
     }
-    DirectoryDCP(Path path) {
-        this.name = path.getFileName();
-        if (!Files.isDirectory(path)) {
+    DirectoryDCP(Path path, boolean recursively) {
+        this.name = path;
+        this.files = Optional.empty();
+        this.dirs = Optional.empty();
+        if (recursively) {
+            if (!Files.isDirectory(path)) {
+                return;
+            } else {
+                DirectoryDCP_(path, this);
+            }
+        } else {
             return;
         }
-//        Directory_(path, this);
-        DirectoryDCP_(path, this);
     }
+
+//    DirectoryDCP(Path path) {
+//        this.name = path.getFileName();
+//        this.files = Optional.empty();
+//        this.dirs = Optional.empty();
+//        if (!Files.isDirectory(path)) {
+//            return;
+//        }
+//        DirectoryDCP_(path, this);
+//    }
+
     public void DirectoryDCP_(Path path, DirectoryDCP ptr) {
         try (Stream<Path> stream = Files.list(path)) {
             stream.forEach(p -> {
                 if (Files.isDirectory(p)) {
-                    DirectoryDCP temp = new DirectoryDCP(p.getFileName().toString());
-                    ptr.dirs.put(p.getFileName(), temp);
+                    DirectoryDCP temp = new DirectoryDCP(p.getFileName(), false);
+                    ptr.addDirectory(temp);
                     DirectoryDCP_(p, temp);
                 } else {
-                    ptr.files.add(p.getFileName());
+                    ptr.addFile(p.getFileName());
                 }
             });
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void addFile(Path file) {
+        this.files.ifPresentOrElse(
+            files -> files.add(file),
+            () -> {
+                Set<Path> files = new HashSet<Path>();
+                this.files = Optional.of(files);
+            }
+        );
+    }
+
+    public void addDirectory(DirectoryDCP dir) {
+        this.dirs.ifPresentOrElse(
+            dirs -> dirs.put(dir.name, dir),
+            () -> {
+                Map<Path, DirectoryDCP> temp = new HashMap<>();
+                temp.put(dir.name, dir);
+                this.dirs = Optional.of(temp);
+            }
+        );
     }
 
     public void print() {
@@ -54,26 +93,15 @@ public class DirectoryDCP implements Comparable<DirectoryDCP> {
     public void print_(DirectoryDCP ptr, int[] indent) {
         printWithIndent(ptr.name.toString(), indent);
         indent[0] += 4;
-        ptr.files.forEach(a -> printWithIndent(a.toString(), indent));
-        for (DirectoryDCP dir : ptr.dirs.values()) print_(dir, indent);
-        indent[0] -= 4;
-    }
-
-    public static void df_function(
-        DirectoryDCP new_dir,
-        DirectoryDCP latest_dir
-    ) {
-         if (new_dir.dirs == null) return;
-
-//         new_dir.files.entrySet().stream()
-    }
-
-    public DirectoryDCP copy_from(DirectoryDCP from) {
-        DirectoryDCP temp = new DirectoryDCP();
-        temp.name = from.name;
-        temp.files = new HashSet<>(from.files);
-        temp.dirs = new HashMap<>(from.dirs);
-        return temp;
+        ptr.files.ifPresent(
+            files -> files.forEach(a -> printWithIndent(a.toString(), indent))
+        );
+        ptr.dirs.ifPresent(
+            dirs -> {
+                for (DirectoryDCP dir : dirs.values()) print_(dir, indent);
+                indent[0] -= 4;
+            }
+        );
     }
 
     @Override
